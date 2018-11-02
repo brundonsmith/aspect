99750e68ddc91be7a8acb77ca76911ba64edfaf7
Brandon Smith <brandon.smith.945@gmail.com>
2018-11-01 20:28:18 -0500

    Cleanup

--- library/classes/Aspect.ts
+++
@@ -1,9 +0,0 @@
-import Selector from "./Selector";
-
-export default abstract class Aspect {
-    selector: Selector;
-
-    constructor(selector: Selector) {
-        this.selector = selector;
-    }
-}
